6864896d6957af96a081807ae9469301cec4e28d
JWardlaw <jwardlaw.n@gmail.com>
2016-01-30 13:12:56 -0800

    Main Menu script in progress. Main Menu Art added.

---
+++ Assets/Scripts/UI/MainMenu_ChangeMenu.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu_ChangeMenu : MonoBehaviour {
+    public GameObject SettingsMenu;
+    public GameObject CreditsMenu;
+    public GameObject MainMenu;
+
+	public void ToggleSettings()
+    {
+        Debug.Log("Toggle S");
+        if(SettingsMenu.activeSelf)
+        {
+            SettingsMenu.SetActive(false);
+            MainMenu.SetActive(true);
+        }
+        else
+        {
+            MainMenu.SetActive(false);
+            SettingsMenu.SetActive(true);
+        }
+    }
+
+    public void ToggleCredits()
+    {
+        Debug.Log("Toggle C");
+        if (CreditsMenu.activeSelf)
+        {
+            CreditsMenu.SetActive(false);
+            MainMenu.SetActive(true);
+        }
+        else
+        {
+            MainMenu.SetActive(false);
+            CreditsMenu.SetActive(true);
+        }
+    }
+}
