46f1bba9ec6d045db25ff13b7e17f1d2ad1b12d0
Reddeyfish <edrichd@uci.edu>
2016-01-30 09:34:29 -0800

    add code for click-in-order game

---
+++ Assets/Scripts/Level/AcupunctureLevel/PunctureLevel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class PunctureLevel : Level {
+
+    [SerializeField]
+    protected int[] puncturePointNumbers;
+
+    Queue<int> sortedPointNumbers;
+
+	// Use this for initialization
+	void Awake () {
+        Array.Sort(puncturePointNumbers);
+        sortedPointNumbers = new Queue<int>(puncturePointNumbers);
+	}
+
+    public bool PlayerActed(int order)
+    {
+        if (order == sortedPointNumbers.Peek())
+        {
+            sortedPointNumbers.Dequeue();
+            PlayerActed();
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
