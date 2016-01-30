82f5b976454dc4dcc029905dc0845e7ffb3e9338
Reddeyfish <edrichd@uci.edu>
2016-01-30 13:36:34 -0800

    added fish

---
+++ Assets/Scripts/Character/AbstractCirclable.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+public class AbstractCirclable : MonoBehaviour, ICirclable
+{
+    public void Notify(Circle cir)
+    {
+        Debug.Log("Circled!");
+    }
+
+    public void Notify(PolygonCollider2D col) { }
+}
