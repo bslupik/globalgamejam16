fe56a0952d633fc04904e27b5ebbb9618bb8b744
Geoffrey Tucker <gtuck123@yahoo.com>
2016-01-30 09:58:48 -0800

    Commented out sound code in OnBeatDraggable

--- Assets/Scripts/Character/OnBeatDraggable.cs
+++ Assets/Scripts/Character/OnBeatDraggable.cs
@@ -23,7 +23,7 @@ public class OnBeatDraggable : Draggable {
         else
         {
             // TODO: Play error sound
-            base.sound.PlaySound(0);
+            // base.sound.PlaySound(0);
         }
     }
 
