95ecdbc038e66cb617e800c1776653be9ac9ac91
Reddeyfish <edrichd@uci.edu>
2016-01-29 21:00:59 -0800

    add draggable script

---
+++ Assets/Scripts/Character/Draggable.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Collider2D))]
+public class Draggable : MonoBehaviour {
+
+    void OnMouseDown()
+    {
+
+        StartCoroutine(DoDrag());
+    }
+
+    IEnumerator DoDrag()
+    {
+        Vector3 relativeMousePos = mousePosInWorld() - this.transform.position;
+
+        while (Input.GetMouseButton(0)) //while being dragged
+        {
+            this.transform.position = mousePosInWorld() - relativeMousePos;
+            yield return null;
+        }
+    }
+
+    public static Vector3 mousePosInWorld()
+    {
+        return mousePosInWorld(Camera.main.transform); //I don't want to use default parameters, because that would involve some extra computation through null-coalescing (the ?? thing)
+    }
+
+    public static Vector3 mousePosInWorld(Transform cameraTransform)
+    {
+        Vector3 screenPoint = Input.mousePosition;
+        screenPoint.z = -cameraTransform.position.z;
+        return screenPoint.toWorldPoint();
+    }
+}
+
