8a4fe204574b9225022e38ede7ea89eb4176253e
Doug314 <douglasbartolme@gmail.com>
2016-01-30 11:46:05 -0800

    fixing compile errors

--- Assets/Scripts/UI/MainMenu_ChangeLevel.cs
+++ Assets/Scripts/UI/MainMenu_ChangeLevel.cs
@@ -1,11 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.SceneManagement;
 
 public class MainMenu_ChangeLevel : MonoBehaviour {
 
 	public void ChangeLevel(int levelindex)
     {
-        SceneManager.LoadScene(levelindex);
+        //SceneManager.LoadScene(levelindex);
     }
 }
