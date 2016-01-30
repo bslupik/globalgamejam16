555c8ca917deee2f16740bffe9f288455c948b28
Reddeyfish <edrichd@uci.edu>
2016-01-29 21:41:06 -0800

    add blit shader for fading out (and in)

---
+++ Assets/Scripts/UI/Blit.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blit : MonoBehaviour {
+
+    [SerializeField]
+    protected Material BlitMaterial;
+
+    Material instantiatedBlitMaterial;
+
+    public float fadeProgress; //0 = no fade, 1  = fade
+
+    void Awake()
+    {
+        instantiatedBlitMaterial = Instantiate(BlitMaterial);
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (fadeProgress == 0)
+        {
+            Graphics.Blit(source, destination);
+        }
+        else
+        {
+            instantiatedBlitMaterial.SetFloat(Tags.ShaderParams.cutoff, fadeProgress);
+            Graphics.Blit(source, destination, instantiatedBlitMaterial);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/SceneChangeScripting.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChangeScripting : MonoBehaviour {
+
+    Blit blit;
+
+	// Use this for initialization
+	void Start () {
+        blit = GetComponent<Blit>();
+
+        //for testing
+        Callback.FireAndForget(FadeOut, 10, this);
+	}
+
+    public void FadeOut()
+    {
+        Callback.DoLerp((float l) => blit.fadeProgress = l, 2, this);
+    }
+}
