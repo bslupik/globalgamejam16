10c8429eeb38851eb830ca51fa5a3b87b3e86976
Ben Slupik <bslupik@uci.edu>
2016-01-31 14:14:20 -0800

    All but 5 mini games working

--- Assets/Scripts/Character/Circle.cs
+++ Assets/Scripts/Character/Circle.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 public class Circle : MonoBehaviour {
 
     [SerializeField]
-    protected float maxLength = 6.0f;
+    protected float maxLength = 8.0f;
 
     PolygonCollider2D col;
 
@@ -23,9 +23,9 @@ public class Circle : MonoBehaviour {
     {
         col = GetComponent<PolygonCollider2D>();
         rend = GetComponent<LineRenderer>();
-        if (maxLength != 6.0f)
+        if (maxLength != 8.0f)
         {
-            maxLength = 6.0f;
+            maxLength = 8.0f;
         }
     }
 
